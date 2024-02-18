787c63078c33c3c831e38dc77342a84fad69e3f4
Electrician25 <94013830+Electrician25@users.noreply.github.com>
2024-02-19 00:18:42 +0300

    Add intification model

---
+++ RestApiBlazor/IntificationModel/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestApiBlazor.IntificationModel
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is requared!")]
+        [EmailAddress(ErrorMessage = "This email is valid!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is requared!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
