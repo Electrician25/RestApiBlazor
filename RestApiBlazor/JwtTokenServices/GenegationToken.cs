d71530f14b508e638e590db8afa55ddc28b54588
Kirill <sosisks888@mail.ru>
2024-02-19 12:18:45 +0300

    add create token function

--- RestApiBlazor/JwtTokenServices/GenegationToken.cs
+++ RestApiBlazor/JwtTokenServices/GenegationToken.cs
@@ -1,15 +1,36 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using RESTapi.Users;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace RestApiBlazor.JwtTokenServices
 {
     public class GenegationToken
     {
-        private string CreateToken()
+        private string CreateToken(User user)
         {
             var key = TokenOptions.GetSymmetricSecurityKey();
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            return "";
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, user.Email)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: TokenOptions.ISSUER,
+                audience: TokenOptions.AUDIENCE,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(2),
+                signingCredentials: credentials
+            );
+
+            var jwtSecurityHandler = new JwtSecurityTokenHandler();
+
+            return jwtSecurityHandler.WriteToken(token);
         }
     }
 }
