9be22679734aa6d908489831675cc82779d1ea3e
Kirill <sosisks888@mail.ru>
2024-02-20 11:28:42 +0300

    fix application context

--- RestApiBlazor/Data/ApplicationContext.cs
+++ RestApiBlazor/Data/ApplicationContext.cs
@@ -14,8 +14,8 @@ namespace RESTapi.Data
                 .Options);
         public ApplicationContext(DbContextOptions options)
             : base(options)
-        {
-        }
+        { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
